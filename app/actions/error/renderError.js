36e4b46f13c628f339b772c0fd3b6d7f74e04488
kulakowka <kulakowka@gmail.com>
2015-11-02 03:45:55 +0300

    Added error handler

--- app/actions/error/renderError.js
+++ app/actions/error/renderError.js
@@ -1,5 +1,9 @@
 export default (err, req, res, next) => {
-  console.error(' ')
-  console.error(err)
-  res.status(500).send(err.message)
+  let error = req.app.get('env') === 'development' ? err : {}
+
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: error
+  })
 }
