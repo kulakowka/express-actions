306ec64633865a76a16b01363f1a6e84118ab796
kulakowka <kulakowka@gmail.com>
2015-11-02 03:15:30 +0300

    Added validation to Model. Added pre save hook to Model;

--- app/models/item.js
+++ app/models/item.js
@@ -3,10 +3,15 @@ import mongoose from '../db'
 var itemSchema = mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  title: String,
-  text: String,
-  user: String
+  title: { type: String, required: true, maxlength: 200, minlength: 5},
+  text: { type: String, required: true, maxlength: 100000, minlength: 5},
+  user: { type: String, required: true},
 })
+
+itemSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
 
 // Look at this http://stackoverflow.com/a/17960532/1018328
 var model = mongoose.model.bind(mongoose)
